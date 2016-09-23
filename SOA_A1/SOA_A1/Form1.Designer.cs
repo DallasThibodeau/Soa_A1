765d12de235350f74d50127f6758ed43dd03104a
wboldt-cc <wboldt-cc@conestogac.on.ca>
2016-09-23 12:57:06 -0400

    First Commit from VS

--- SOA_A1/SOA_A1/Form1.Designer.cs
+++ SOA_A1/SOA_A1/Form1.Designer.cs
@@ -29,7 +29,9 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.ddlWebService = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ddlWebMethod = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
@@ -41,23 +43,45 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Select the Web Service:";
             // 
-            // comboBox1
+            // ddlWebService
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.ddlWebService.FormattingEnabled = true;
+            this.ddlWebService.Items.AddRange(new object[] {
             "Test 1",
             "Last test"});
-            this.comboBox1.Location = new System.Drawing.Point(142, 13);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(146, 21);
-            this.comboBox1.TabIndex = 1;
+            this.ddlWebService.Location = new System.Drawing.Point(142, 13);
+            this.ddlWebService.Name = "ddlWebService";
+            this.ddlWebService.Size = new System.Drawing.Size(146, 21);
+            this.ddlWebService.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select the Method:";
+            // 
+            // ddlWebMethod
+            // 
+            this.ddlWebMethod.FormattingEnabled = true;
+            this.ddlWebMethod.Items.AddRange(new object[] {
+            "Method 1",
+            "Method 2"});
+            this.ddlWebMethod.Location = new System.Drawing.Point(142, 56);
+            this.ddlWebMethod.Name = "ddlWebMethod";
+            this.ddlWebMethod.Size = new System.Drawing.Size(146, 21);
+            this.ddlWebMethod.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(604, 281);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.ddlWebMethod);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ddlWebService);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "SOA A1 - William Boldt and Dallas Thibodeau";
@@ -69,7 +93,9 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox ddlWebService;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox ddlWebMethod;
     }
 }
 
