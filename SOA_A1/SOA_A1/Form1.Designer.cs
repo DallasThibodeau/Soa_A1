2843737bf1ce1bb23cce0778be883ae69897066b
Dallas Thibodeau <fusebox45@gmail.com>
2016-09-28 02:10:03 -0400

    Implemented one method. Mega easy. If you want to figure out how to populate the list dynamically I can figure out how to dynamically display returns.
    Has literally zero error checking.

--- SOA_A1/SOA_A1/Form1.Designer.cs
+++ SOA_A1/SOA_A1/Form1.Designer.cs
@@ -32,14 +32,19 @@
             this.ddlWebService = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.ddlWebMethod = new System.Windows.Forms.ComboBox();
+            this.GoButton = new System.Windows.Forms.Button();
+            this.ReturnLabel = new System.Windows.Forms.Label();
+            this.Argument = new System.Windows.Forms.Label();
+            this.argumentBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 16);
+            this.label1.Location = new System.Drawing.Point(17, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(123, 13);
+            this.label1.Size = new System.Drawing.Size(159, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Select the Web Service:";
             // 
@@ -47,19 +52,20 @@
             // 
             this.ddlWebService.FormattingEnabled = true;
             this.ddlWebService.Items.AddRange(new object[] {
-            "Test 1",
-            "Last test"});
-            this.ddlWebService.Location = new System.Drawing.Point(142, 13);
+            "Number Conversions"});
+            this.ddlWebService.Location = new System.Drawing.Point(189, 16);
+            this.ddlWebService.Margin = new System.Windows.Forms.Padding(4);
             this.ddlWebService.Name = "ddlWebService";
-            this.ddlWebService.Size = new System.Drawing.Size(146, 21);
+            this.ddlWebService.Size = new System.Drawing.Size(193, 24);
             this.ddlWebService.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 60);
+            this.label2.Location = new System.Drawing.Point(21, 74);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.Size = new System.Drawing.Size(126, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "Select the Method:";
             // 
@@ -67,22 +73,63 @@
             // 
             this.ddlWebMethod.FormattingEnabled = true;
             this.ddlWebMethod.Items.AddRange(new object[] {
-            "Method 1",
-            "Method 2"});
-            this.ddlWebMethod.Location = new System.Drawing.Point(142, 56);
+            "Numbers to Words"});
+            this.ddlWebMethod.Location = new System.Drawing.Point(189, 69);
+            this.ddlWebMethod.Margin = new System.Windows.Forms.Padding(4);
             this.ddlWebMethod.Name = "ddlWebMethod";
-            this.ddlWebMethod.Size = new System.Drawing.Size(146, 21);
+            this.ddlWebMethod.Size = new System.Drawing.Size(193, 24);
             this.ddlWebMethod.TabIndex = 3;
+            // 
+            // GoButton
+            // 
+            this.GoButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.GoButton.Location = new System.Drawing.Point(307, 178);
+            this.GoButton.Name = "GoButton";
+            this.GoButton.Size = new System.Drawing.Size(75, 23);
+            this.GoButton.TabIndex = 4;
+            this.GoButton.Text = "Go";
+            this.GoButton.UseVisualStyleBackColor = true;
+            this.GoButton.Click += new System.EventHandler(this.GoButton_Click);
+            // 
+            // ReturnLabel
+            // 
+            this.ReturnLabel.AutoSize = true;
+            this.ReturnLabel.Location = new System.Drawing.Point(17, 219);
+            this.ReturnLabel.Name = "ReturnLabel";
+            this.ReturnLabel.Size = new System.Drawing.Size(0, 17);
+            this.ReturnLabel.TabIndex = 5;
+            // 
+            // Argument
+            // 
+            this.Argument.AutoSize = true;
+            this.Argument.Location = new System.Drawing.Point(21, 136);
+            this.Argument.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Argument.Name = "Argument";
+            this.Argument.Size = new System.Drawing.Size(73, 17);
+            this.Argument.TabIndex = 6;
+            this.Argument.Text = "Argument:";
+            // 
+            // argumentBox
+            // 
+            this.argumentBox.Location = new System.Drawing.Point(189, 130);
+            this.argumentBox.Name = "argumentBox";
+            this.argumentBox.Size = new System.Drawing.Size(193, 22);
+            this.argumentBox.TabIndex = 7;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(604, 281);
+            this.ClientSize = new System.Drawing.Size(805, 346);
+            this.Controls.Add(this.argumentBox);
+            this.Controls.Add(this.Argument);
+            this.Controls.Add(this.ReturnLabel);
+            this.Controls.Add(this.GoButton);
             this.Controls.Add(this.ddlWebMethod);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.ddlWebService);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "SOA A1 - William Boldt and Dallas Thibodeau";
             this.ResumeLayout(false);
@@ -96,6 +143,10 @@
         private System.Windows.Forms.ComboBox ddlWebService;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox ddlWebMethod;
+        private System.Windows.Forms.Button GoButton;
+        private System.Windows.Forms.Label ReturnLabel;
+        private System.Windows.Forms.Label Argument;
+        private System.Windows.Forms.TextBox argumentBox;
     }
 }
 
--- SOA_A1/SOA_A1/Form1.cs
+++ SOA_A1/SOA_A1/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Windows.Forms;
 
@@ -14,6 +16,46 @@ namespace SOA_A1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void GoButton_Click(object sender, EventArgs e)
+        {
+            string postUrl = "http://www.dataaccess.com/webservicesserver/numberconversion.wso";
+            string DataToSend = @"<?xml version=""1.0"" encoding=""UTF-8""?><soap:Envelope xmlns:soap=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:tns=""http://www.dataaccess.com/webservicesserver/"" xmlns:xs=""http://www.w3.org/2001/XMLSchema""><soap:Body><tns:NumberToWords><tns:ubiNum>" + argumentBox.Text + "</tns:ubiNum></tns:NumberToWords></soap:Body></soap:Envelope>";
+            string responseFromServer = "";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(postUrl);
+            request.Headers.Add("SOAPAction", "\" " + postUrl + "\"");
+            request.ContentType = "text/xml;charset=\"utf-8\"";
+            request.Accept = "text/xml";
+            request.Method = "POST";
+
+            Stream stm = request.GetRequestStream();
+
+            //write the data to the stream aquired from request
+            using (StreamWriter stmw = new StreamWriter(stm))
+            {
+                stmw.Write(DataToSend);
+            }
+
+            //get the response from the web service
+            WebResponse response;
+            response = request.GetResponse();
+
+            stm.Close();
+
+            stm = response.GetResponseStream();
+
+            //parse out any information in the services response
+            using (StreamReader stmr = new StreamReader(stm))
+            {
+                ReturnLabel.Text = "";
+                while (stmr.EndOfStream == false)
+                {
+                    responseFromServer = stmr.ReadLine();
+                    ReturnLabel.Text += responseFromServer + "<br/>";
+                }
+            }
         }
     }
 }
